d60000cf544ea2752573b7bcea0408d1c7cf7dd4
Andrew Ray <me@andrewray.me>
2023-05-10 22:30:37 +0800

    Removing examples from core

--- src/plugins/babylon/index.ts
+++ src/plugins/babylon/index.ts
@@ -1,6 +1,3 @@
 import { babylengine } from './bablyengine';
-import BabylonComponent from './BabylonComponent';
 
-export { BabylonComponent as Editor, babylengine as engine };
-
-export * from './examples';
+export { babylengine as engine };
--- src/plugins/three/index.ts
+++ src/plugins/three/index.ts
@@ -1,6 +1,3 @@
 import { threngine } from './threngine';
-import ThreeComponent from './ThreeComponent';
 
-export { ThreeComponent as Editor, threngine as engine };
-
-export * from './examples';
+export { threngine as engine };
