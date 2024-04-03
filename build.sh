2b81ed0863db3bf646bf796fe66c838a8bcfdeee
Andrew Ray <me@andrewray.me>
2024-04-02 22:32:55 -0700

    who knows

--- build.sh
+++ build.sh
@@ -2,8 +2,10 @@
 set -e
 
 # https://evertpot.com/universal-commonjs-esm-typescript-packages/
-npx tsc --module commonjs --outDir cjs/
-echo '{"type": "commonjs"}' > cjs/package.json
+# npx tsc --module commonjs --outDir cjs/
+# echo '{"type": "commonjs"}' > cjs/package.json
 
-npx tsc --module es2022 --outDir esm/
-echo '{"type": "module"}' > esm/package.json
+# npx tsc --module es2022 --outDir esm/
+# echo '{"type": "module"}' > esm/package.json
+
+npx tsc
--- postbuild.sh
+++ postbuild.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
 
-rm -rf cjs esm
+# rm -rf cjs esm
+rm -rf engine.d.ts engine.js graph/ index.d.ts index.html index.js plugins/ strategy/ util/
