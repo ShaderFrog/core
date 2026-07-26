169e1de2c8604e951afaac0f9bed6a31ad36ddfa
Andrew Ray <me@andrewray.me>
2026-07-26 12:00:50 -0700

    test

--- src/plugins/three/index.ts
+++ src/plugins/three/index.ts
@@ -1,3 +1,5 @@
 import { createMaterial, threngine } from './threngine';
 
 export { createMaterial, threngine as engine };
+export { FrogMaterial, expandChunks, replaceFromOffset, replaceLast } from './FrogMaterial';
+export type { FrogMaterialParams } from './FrogMaterial';
